2a49ebd3a701aa38f57e6fa64fb3c2813defa81f
akzholtech-kz <akzhol.nuradin@nu.edu.kz>
2022-04-05 11:56:05 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
+import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
